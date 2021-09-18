12b4eaa7158a724a63c8e885a1ca789001161150
Thezero <67761713+Thezero27@users.noreply.github.com>
2021-09-18 11:48:32 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var clickYepeta = document.getElementById("yepeta-click")
 clickYepeta.onclick = function (){
     $("#yepeta-img").html("<img class='imgw' src='img/anonymous16.jpg'>")
     $("#yepeta-name").html("<span style='display: block;' class='card-text'>Angel</span>")
-    $("#yepeta-aviso").html("<span class='card-text'>+51 925529690</span>")
-    $("descarga").html("<span id='descarga' href='https://wa.link/x9yb7f'>Descargar</span>")
+    $("#yepeta-aviso").html("<span class='card-text'>+51 925******</span>")
+    $("descarga").html("<span id='descarga' href='https://wa.link/x9yb7fky'>Descargar</span>")
 
 }
